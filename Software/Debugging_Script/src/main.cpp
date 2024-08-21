17acdff23d3f3e432d6040d723751a6049704c80
Marshmelllo <edme0001@student.monash.edu>
2024-08-21 21:18:37 +1000

    Updated Rotary Configuration and added Macro Layers
    Updated the rotary implimentation across all projects so that the correct pins are being activated.
    
    Switched to HID from Arduinos Keyboard.h as it couldnt control media systems such as volume, now with the HID library media controls work across multiple devices.
    
    Started the implimentation of Layerd macros so that users can change macros based on device or use.

--- Software/Debugging_Script/src/main.cpp
+++ Software/Debugging_Script/src/main.cpp
@@ -12,13 +12,12 @@
  */
 
 #include <Arduino.h>
-#include <Keyboard.h>
 #include <Encoder.h>
+#include <HID-Project.h>
 
 const int numRows = 4;
 const int numCols = 5;
 
-// Button layout based on your numbering scheme
 const char* keys[numRows][numCols] = {
   {0, "4", "8", "12", "16"},
   {0, "3", "7", "11", "15"},
@@ -29,12 +28,8 @@ const char* keys[numRows][numCols] = {
 const int rowPins[numRows] = {A3, A2, A1, A0};
 const int colPins[numCols] = {5, 6, 7, 8, 9};
 
-#define ENCODER_PIN1 2
-#define ENCODER_PIN2 3
-
-// Define multimedia key codes for volume control
-#define VOLUME_UP 0x80  
-#define VOLUME_DOWN 0x81  
+#define ENCODER_PIN1 16
+#define ENCODER_PIN2 4
 
 const unsigned long debounceTime = 100;
 
@@ -62,7 +57,8 @@ void setup() {
     pinMode(colPins[i], INPUT_PULLUP);
   }
 
-  Keyboard.begin();
+  BootKeyboard.begin();
+  Consumer.begin(); // Initialize the ConsumerControl (for media keys)
 }
 
 void loop() {
@@ -75,7 +71,7 @@ void loop() {
       if (digitalRead(colPins[col]) == LOW) { // Button pressed
         if (keyState[row][col] == false && (currentTime - lastDebounceTime[row][col] > debounceTime)) {
           keyState[row][col] = true; // Mark the key as pressed
-          Keyboard.print(keys[row][col]); // Send the corresponding number via keyboard
+          BootKeyboard.print(keys[row][col]); // Send the corresponding number via keyboard
           lastDebounceTime[row][col] = currentTime; // Update debounce time
         }
       } else { // Button released
@@ -91,13 +87,9 @@ void loop() {
   long newPosition = myEnc.read() / 4;
   if (newPosition != oldPosition) {
     if (newPosition > oldPosition) {
-      Keyboard.press(VOLUME_UP); // Increase volume
-      delay(100);
-      Keyboard.release(VOLUME_UP);
+      Consumer.write(MEDIA_VOLUME_UP);  // Increase volume
     } else {
-      Keyboard.press(VOLUME_DOWN); // Decrease volume
-      delay(100);
-      Keyboard.release(VOLUME_DOWN);
+      Consumer.write(MEDIA_VOLUME_DOWN);  // Decrease volume
     }
     oldPosition = newPosition;
   }
